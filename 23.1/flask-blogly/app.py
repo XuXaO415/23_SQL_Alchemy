cc8ebeb7bbcd82fd913d9f980379964bcfc84171
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-08-25 18:33:55 -0700

    Added setup for this project

--- 23.1/flask-blogly/app.py
+++ 23.1/flask-blogly/app.py
@@ -1,12 +1,20 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "011100110110010101100011011100100110010101110100"
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+# @app.route('/')
+# def home_page():
+#     """Displays homepage"""
--- 23.1/flask-blogly/models.py
+++ 23.1/flask-blogly/models.py
@@ -1,1 +1,25 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    
+#Models
+    
+    class User(db.Model):
+        __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    first_name = db.column(db.Text, nullable=False)
+    
+    last_name = db.column(db.Text, nullable=False)
+    
+    image_url = db.column(db.text, nullable=False)
+    
+    
