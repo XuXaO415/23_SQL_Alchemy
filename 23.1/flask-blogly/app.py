e10c8d568f468e89e254aa8f4e431613b5b762c8
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-08-26 21:44:07 -0700

    Added content

--- 23.1/flask-blogly/app.py
+++ 23.1/flask-blogly/app.py
@@ -1,15 +1,17 @@
 """Blogly application."""
 
-from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, render_template, redirect, session
 from models import db, connect_db, User
 
+# createdb blogly_db
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "011100110110010101100011011100100110010101110100"
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "0111001101100101011"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -28,13 +30,17 @@ def list_users():
     users = User.query.all()
     return render_template('user_list.html', users=users)
 
-@app.route('/<int:user_id>')
-def show_user(user_id):
-    """Show details about a single user"""
-    user = User.query.get(user_id)
-    # return f"<h1>{user.first_name}</h1>"
-    user = User.query.get_or_404(user_id)
-    return render_template('user_details.html', user=user)
+# @app.route('/', methods=["POST"])
+# def create_user():
+#     return "Your added you info"
+
+# @app.route('/<int:user_id>')
+# def show_user(user_id):
+#     """Show details about a single user"""
+#     user = User.query.get(user_id)
+#     # return f"<h1>{user.first_name}</h1>"
+#     user = User.query.get_or_404(user_id)
+#     return render_template('user_details.html', user=user)
     
     
     
--- 23.1/flask-blogly/models.py
+++ 23.1/flask-blogly/models.py
@@ -1,32 +1,49 @@
 """Models for Blogly."""
 
+# createdb blogly_db
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
 def connect_db(app):
+    """Connect to database."""
     db.app = app
     db.init_app(app)
-    
-#Models
-    
-class User(db.Model):
-        """User."""
-        __tablename__ = 'users'
-        
-        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-        first_name = db.column(db.Text(50), nullable=False)
-    
-        last_name = db.column(db.Text(50), nullable=False)
-    
-        image_url = db.column(db.Text(50), nullable=True)
-    
-    
-        def __repr__(self):
-            """Show info about user"""
-        
-            u = self
-            return f"<User {u.id} {u.first_name} {u.last_name} {u.image_url}>"
-    
+# User table
+class User(db.Model):
+    """User."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    first_name = db.Column(db.String(50), nullable=False)
+
+    last_name = db.Column(db.String(50), nullable=False)
+
+    image_url = db.Column(db.String(50), nullable=True)
+
+    def __repr__(self):
+        """Show info about user"""
+
+        u = self
+        return f"<User {u.id} {u.first_name} {u.last_name} {u.image_url}>"2021-08-26 21: 17: 13, 596 INFO sqlalchemy.engine.Engine
+
+
+# 2021-08-26 21: 17: 13, 596 INFO sqlalchemy.engine.Engine
+# CREATE TABLE users(
+# 	id SERIAL NOT NULL,
+# 	first_name VARCHAR(50) NOT NULL,
+# 	last_name VARCHAR(50) NOT NULL,
+# 	image_url VARCHAR(50),
+# 	PRIMARY KEY(id)
+# )
+
+You are now connected to database "blogly_db" as user "......".
+blogly_db =  # SELECT * FROM users;
+id | first_name | last_name | image_url
+----+------------+-----------+-----------
+(0 rows)
