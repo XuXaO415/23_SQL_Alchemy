bffaee934280c935f29ac8795508a371341e1634
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-09-12 17:15:41 -0700

    Actively working on this project

--- 23.1/flask-blogly/app.py
+++ 23.1/flask-blogly/app.py
@@ -265,7 +265,7 @@ def delete_tag(tag_id):
 
 
 
-
+#Notes:
 # request.args: the key/value pairs in the URL query string
 # request.form: the key/value pairs in the body, from a HTML post form, or JavaScript request that isn't JSON encoded
 # request.files: the files in the body, which Flask keeps separate from form. HTML forms must use enctype =multipart/form-data or files will not be uploaded.
